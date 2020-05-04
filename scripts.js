6cdc596884753c15afa35e5a781737261eec9267
kinga191118 <64659739+kinga191118@users.noreply.github.com>
2020-05-04 20:01:31 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 // Third part
+console.log('Script file is loaded.');
 
 $('h1').text('This is done by JS.');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
 $('h2').toggleClass('blue');
 
-console.log('Script file is loaded.');
+function myLittleFunction() {
+    $('main').append('<p>A p is appended in the main.</p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+}
+
+myLittleFunction();
+myLittleFunction();
+
+$('add').click(myLittleFunction);
+$('#remove').click(function () {
+    $('p').remove();
+})
 
 // Single line comment
 
