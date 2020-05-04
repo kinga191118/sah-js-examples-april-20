a5c86e08809930a83f4f7a442fbafb1564e9ee74
kinga191118 <64659739+kinga191118@users.noreply.github.com>
2020-05-04 16:59:01 +0200

    add jquery first examples

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+// Third part
+
+$('h1').text('This is done by JS.');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
+
 console.log('Script file is loaded.');
 
 // Single line comment
